e9b4f8de19782a6cccb653f5eb107e150c8e7644
nitinguptamca <nitinguptamca@gmail.com>
2023-04-08 19:47:22 +0530

    resolve all bug..

--- cinema-service/cinema-container/src/main/resources/init-data.sql
+++ cinema-service/cinema-container/src/main/resources/init-data.sql
@@ -1,7 +1,7 @@
 INSERT INTO cinema.cinemas(id, name, active)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb45', 'cinema.1', TRUE);
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb45', 'cinema_1', TRUE);
 INSERT INTO cinema.cinemas(id, name, active)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb46', 'cinema.2', FALSE);
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb46', 'cinema_2', FALSE);
 
 INSERT INTO cinema.movies(id, name, price, available)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb47', 'movie_1', 25.00, FALSE);
@@ -12,11 +12,11 @@ INSERT INTO cinema.movies(id, name, price, available)
 INSERT INTO cinema.movies(id, name, price, available)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb50', 'movie_4', 40.00, TRUE);
 
-INSERT INTO cinema.cinema.movies(id, cinema.id, movie_id)
+INSERT INTO cinema.cinema_movies(id, cinema_id, movie_id)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb51', 'd215b5f8-0249-4dc5-89a3-51fd148cfb45', 'd215b5f8-0249-4dc5-89a3-51fd148cfb47');
-INSERT INTO cinema.cinema.movies(id, cinema.id, movie_id)
+INSERT INTO cinema.cinema_movies(id, cinema_id, movie_id)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb52', 'd215b5f8-0249-4dc5-89a3-51fd148cfb45', 'd215b5f8-0249-4dc5-89a3-51fd148cfb48');
-INSERT INTO cinema.cinema.movies(id, cinema.id, movie_id)
+INSERT INTO cinema.cinema_movies(id, cinema_id, movie_id)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb53', 'd215b5f8-0249-4dc5-89a3-51fd148cfb46', 'd215b5f8-0249-4dc5-89a3-51fd148cfb49');
-INSERT INTO cinema.cinema.movies(id, cinema.id, movie_id)
+INSERT INTO cinema.cinema_movies(id, cinema_id, movie_id)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb54', 'd215b5f8-0249-4dc5-89a3-51fd148cfb46', 'd215b5f8-0249-4dc5-89a3-51fd148cfb50');
